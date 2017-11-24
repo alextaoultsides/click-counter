42db98298fe5a94fc14ec20bdac98410d23c104c
alextaoultsides <alextaoultsides@gmail.com>
2017-11-24 12:20:51 -0500

    First Commit

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //label
+        var label = UILabel()
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        
+        self.label = label
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Increment", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+        
+        var button1 = UIButton()
+        button1.frame = CGRect.init(x: 150, y: 350, width: 60, height: 60)
+        button1.setTitle("Decrement", for: .normal)
+        button1.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button1)
+        
+        button1.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func incrementCount(){
+        
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
-
+    
+    @objc func decrementCount(){
+        
+        self.count -= 1
+        self.label.text = "\(self.count)"
+    }
+   
 
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# Click Counter
+iOS app 
